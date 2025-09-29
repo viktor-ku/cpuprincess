972a4b4bda539f8c1c92c1f248316e8586ae7e10
Viktor Kuroljov <viktor@ku.family>
2025-09-29 15:59:12 +0300

    remove the CPU percentage leaving only the emoji

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,33 @@
 use std::thread::sleep;
-use sysinfo::{CpuRefreshKind, MemoryRefreshKind, RefreshKind, System};
+use sysinfo::{CpuRefreshKind, RefreshKind, System};
 
+const EMOJI_QUESTION: &str = "❓";
 const EMOJI_CPU_1: &str = "💤";
 const EMOJI_CPU_2: &str = "🎮";
 const EMOJI_CPU_3: &str = "🔥";
 
-const C_GIB_BYTES_DEL: f64 = 1_073_741_824.0;
-
 fn main() {
     let mut sys = System::new_with_specifics(
-        RefreshKind::nothing()
-            .with_cpu(CpuRefreshKind::everything().without_frequency())
-            .with_memory(MemoryRefreshKind::nothing().with_ram()),
+        RefreshKind::nothing().with_cpu(CpuRefreshKind::everything().without_frequency()),
     );
 
-    sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL);
+    sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL.mul_f32(2.0));
 
     sys.refresh_cpu_usage();
     let cpu = sys.global_cpu_usage().ceil() as u64;
 
-    sys.refresh_memory();
-    let used_bytes = sys.used_memory();
-
     let cpu_formatted = fmt_cpu(cpu);
-    let ram_formatted = fmt_memory(used_bytes);
 
     println!("{}", cpu_formatted);
 }
 
-fn fmt_memory(bytes: u64) -> String {
-    let gib = (bytes as f64) / C_GIB_BYTES_DEL;
-
-    let g = gib.floor();
-
-    format!("{} GiB", g)
-}
-
 fn fmt_cpu(usage: u64) -> String {
     let emoji = match usage {
-        0..=9 => EMOJI_CPU_1,
-        10..=89 => EMOJI_CPU_2,
-        90..=100 => EMOJI_CPU_3,
-        _ => "",
+        0..20 => EMOJI_CPU_1,
+        20..85 => EMOJI_CPU_2,
+        85..=100 => EMOJI_CPU_3,
+        _ => EMOJI_QUESTION,
     };
 
-    format!("{usage:2}% {emoji}")
+    format!("{emoji}")
 }
