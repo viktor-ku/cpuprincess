b5e24794587bd39c3344da9a363792c886eabb17
Viktor Kuroljov <viktor@ku.family>
2025-09-30 15:49:08 +0300

    add many fires

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,40 @@
-use std::thread::sleep;
+use std::{io::Write, thread::sleep};
 use sysinfo::{CpuRefreshKind, RefreshKind, System};
 
 const EMOJI_QUESTION: &str = "❓";
-const EMOJI_CPU_1: &str = "💤";
-const EMOJI_CPU_2: &str = "🎮";
-const EMOJI_CPU_3: &str = "🔥";
+const EMOJI_ZZZ: &str = "💤";
+const EMOJI_GAME: &str = "🎮";
+const EMOJI_FIRE: &str = "🔥";
 
 fn main() {
     let mut sys = System::new_with_specifics(
         RefreshKind::nothing().with_cpu(CpuRefreshKind::everything().without_frequency()),
     );
 
-    sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL.mul_f32(2.0));
+    sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL);
 
     sys.refresh_cpu_usage();
-    let cpu = sys.global_cpu_usage().ceil() as u64;
 
-    let cpu_formatted = fmt_cpu(cpu);
+    let cpu = sys.global_cpu_usage();
+    let mut status = String::with_capacity(32);
 
-    println!("{}", cpu_formatted);
+    add_cpu_usage(cpu, &mut status);
+
+    let mut stdout = std::io::stdout();
+
+    stdout.write(status.as_bytes()).unwrap();
+    stdout.flush().unwrap();
 }
 
-fn fmt_cpu(usage: u64) -> String {
-    let emoji = match usage {
-        0..20 => EMOJI_CPU_1,
-        20..85 => EMOJI_CPU_2,
-        85..=100 => EMOJI_CPU_3,
+fn add_cpu_usage(usage: f32, status: &mut String) {
+    let emoji = match usage as u64 {
+        0..20 => EMOJI_ZZZ,
+        20..70 => EMOJI_GAME,
+        70..80 => EMOJI_FIRE,
+        80..90 => &EMOJI_FIRE.repeat(2),
+        90..=100 => &EMOJI_FIRE.repeat(3),
         _ => EMOJI_QUESTION,
     };
 
-    format!("{emoji}")
+    status.push_str(emoji);
 }
